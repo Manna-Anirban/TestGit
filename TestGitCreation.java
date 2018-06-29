8c683da9edd65ee7e58dbdcea8422ff6651b397e
Manna-Anirban <amanna@cpp.edu>
2018-06-29 14:19:06 -0700

    Java class created from Git Bash

---
+++ TestGitCreation.java
@@ -0,0 +1,11 @@
+public class TestGitCreation 
+{
+ public static void main(String[] args)
+ {
+       System.out.println();
+       System.out.println("Testing a java class " +
+                       +"created solely through Git Bash"); 
+       System.out.println(); 
+
+ }
+}
